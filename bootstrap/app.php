1205150ef6a25c7aa7b1c7b47b93236f553c9b3a
engrpaim <joseephraimbatacan@gmail.com>
2026-01-05 20:00:55 +0800

    csrf issuev2.1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,14 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            \Illuminate\Cookie\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
-
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
