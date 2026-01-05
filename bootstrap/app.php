12fddb21b7933d76931169fd1b056c715c3129a1
engrpaim <joseephraimbatacan@gmail.com>
2026-01-05 20:29:29 +0800

    port proxy issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
+
+        // Add this line:
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
