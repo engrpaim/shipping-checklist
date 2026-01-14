119ff58f77d1866a2f3ce095af460f5dc94283c6
engrpaim <joseephraimbatacan@gmail.com>
2026-01-15 04:07:15 +0800

    13try1

--- app/Http/Controllers/DataManipulationController.php
+++ app/Http/Controllers/DataManipulationController.php
@@ -437,29 +437,14 @@ class DataManipulationController extends Controller
     $displayPreview = [];
     $finalPreview = [];
     $files = ['container.jpg', 'container.png','pallets.jpg','pallets.jpg','slip.png','slip.jpg','seal.png','seal.jpg'];
-    foreach($getAllBooked as $key => $value){
-        if(!$value->Shipment_Serial && !$value->Forwarder) return;
-        $serial = $value->Shipment_Serial;
-        $displayPreview [$serial]['Forwarder']=$value->Forwarder;
-        $queueDisplay = DataManipulationController::pullDataBooking($serial);
-        $finalPreview [$serial]= array_merge($queueDisplay[$serial] ,  $displayPreview[$serial]);
-        $getDGstatus = DataManipulationController::pullDataGrabbers($serial);
-        $finalPreview [$serial]['Shipment_Status'] = $getDGstatus;
-
-        $checkingShipmentFolder = $year ."/". $value->Shipment_Serial;
-
-        if(!file_exists($checkingShipmentFolder)){
-          mkdir($checkingShipmentFolder,0775,true);
-        }
-        $pictureStatus =  $checkingShipmentFolder. "/"  ;
-        foreach($files as $picture){
-            str_contains($picture,'container') ? $finalPreview[$serial]['container_picture'] = (file_exists($pictureStatus.'container.jpg')||file_exists($pictureStatus.'container.png')):null;
-            str_contains($picture,'slip') ? $finalPreview[$serial]['slip_picture'] = (file_exists($pictureStatus.'slip.jpg')||file_exists($pictureStatus.'slip.png')):null;
-            str_contains($picture,'seal') ? $finalPreview[$serial]['seal_picture'] = (file_exists($pictureStatus.'seal.jpg')||file_exists($pictureStatus.'seal.png')):null;
-            str_contains($picture,'pallets') ? $finalPreview[$serial]['pallets_picture'] = (file_exists($pictureStatus.'pallets.jpg')||file_exists($pictureStatus.'pallets.png')):null;
-        }
-
+    $pictureStatus =  $year. "/"  ;
+    foreach($files as $picture){
+        str_contains($picture,'container') ? $finalPreview[$serial]['container_picture'] = (file_exists($pictureStatus.'container.jpg')||file_exists($pictureStatus.'container.png')):null;
+        str_contains($picture,'slip') ? $finalPreview[$serial]['slip_picture'] = (file_exists($pictureStatus.'slip.jpg')||file_exists($pictureStatus.'slip.png')):null;
+        str_contains($picture,'seal') ? $finalPreview[$serial]['seal_picture'] = (file_exists($pictureStatus.'seal.jpg')||file_exists($pictureStatus.'seal.png')):null;
+        str_contains($picture,'pallets') ? $finalPreview[$serial]['pallets_picture'] = (file_exists($pictureStatus.'pallets.jpg')||file_exists($pictureStatus.'pallets.png')):null;
     }
+
 
     return Inertia::render('Main', [
         'appName' => config('app.name'),
