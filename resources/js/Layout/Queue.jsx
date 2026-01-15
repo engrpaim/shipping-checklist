00f9ddefa9e2e23cd6f7b1e89dac980da23832fc
engrpaim <joseephraimbatacan@gmail.com>
2026-01-16 03:09:28 +0800

    new try

--- resources/js/Layout/Queue.jsx
+++ resources/js/Layout/Queue.jsx
@@ -429,6 +429,8 @@ export default function Queue(queueData){
                                                 />
                                             </div>
                                         )}
+
+
                                     </div>
                                 </>:null
                                 }
