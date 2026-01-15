29ddd0b97c7175e26d4974d6d0a6584a5ac42c59
engrpaim <joseephraimbatacan@gmail.com>
2026-01-15 18:52:53 +0800

    new try

--- routes/web.php
+++ routes/web.php
@@ -108,12 +108,12 @@ Route::get('/shipping-checklist/queue', function (Request $request) {
                         : null);
             }
 
-            // Pallets picture
+            $pallet_image = null;
             if (str_contains($picture, 'pallets')) {
                 $finalPreview[$serial]['pallets_image'] = File::exists($pictureStatus.'pallets.jpg')
-                    ? $pictureStatus.'pallets.jpg'
+                    ?  $pallet_image = File::get($pictureStatus.'pallets.jpg')
                     : (File::exists($pictureStatus.'pallets.png')
-                        ? $pictureStatus.'pallets.png'
+                        ? $pallet_image = File::get($pictureStatus.'pallets.png')
                         : null);
 
             }
