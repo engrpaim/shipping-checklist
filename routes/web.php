058038c62aa77edfa6bc46f86d5f0084a43d4fce
engrpaim <joseephraimbatacan@gmail.com>
2026-01-20 20:53:39 +0800

    try upload

--- routes/web.php
+++ routes/web.php
@@ -38,8 +38,8 @@ Route::get('/shipping-checklist/booking', function (Request $request) {
 Route::get('/shipping-checklist/queue', function (Request $request) {
     $ip = $request->ip();
     $check = Admin::where('ip_address','=',$ip)->first();
-    // $path = 'C:/Shipping_Check_List';
-    $path = '/var/data/Shipping_Check_List';
+    $path = 'C:/Shipping_Check_List';
+    // $path = '/var/data/Shipping_Check_List';
     $currentYear = Carbon::now()->year;
     $yearPath = $path ."/". $currentYear;
     if(!File::exists($path)){
@@ -126,7 +126,7 @@ Route::get('/shipping-checklist/queue', function (Request $request) {
         'appName' => config('app.name'),
         'page' => 'queue',
         'queue' =>  $finalPreview,
-        'client_ip' => $check->ip_address?? null,
+        'client_ip' => $check->ip_address ?? null,
         'client_details' => $check ?? null
     ]);
 });
